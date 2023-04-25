db6c9664d093a508a7db831752b9151371ad7e24
AnilAydemir <cazorte@gmail.com>
2023-04-25 15:49:38 +0300

    cards created

--- src/test/java/com/apiTestTrello/ApiTest.java
+++ src/test/java/com/apiTestTrello/ApiTest.java
@@ -6,6 +6,11 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class ApiTest {
 
 
@@ -25,7 +30,7 @@ public class ApiTest {
                         when()
                         .queryParam("key", ConfigReader.get("key"))
                         .queryParam("token", ConfigReader.get("token"))
-                        .queryParam("name", "BoardAnil")
+                        .queryParam("name", "BoardAnil2")
                         .post("boards")
                         .then()
                         .statusCode(200)
@@ -34,6 +39,44 @@ public class ApiTest {
                         .extract().path("id");
 
         System.out.println("boardID = " + boardID);
+
+        String listId =
+                given()
+                        .contentType("application/json")
+                        .when()
+                        .queryParam("key", ConfigReader.get("key"))
+                        .queryParam("token", ConfigReader.get("token"))
+                        .queryParam("name","TrelloList")
+                        .post("/boards/"+boardID+"/lists")
+                        .then()
+                        .statusCode(200)
+                        .contentType(ContentType.JSON).log().all()
+                        .assertThat()
+                        .body("name", equalTo("TrelloList"))
+                        .body("idBoard", equalTo(boardID))
+                        .extract().path("id");
+
+        String[] cardsIdArr = new String[2];
+
+
+        for (int i = 0; i < 2; i++) {
+            cardsIdArr[i] = given()
+                    .contentType("application/json").
+                    when()
+                    .queryParam("key", ConfigReader.get("key"))
+                    .queryParam("token", ConfigReader.get("token"))
+                    .queryParam("name","newCard"+i)
+                    .queryParam("idList",listId)
+                    .post("/cards").
+                    then()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON).
+                    assertThat()
+                    .body("name", equalTo("newCard"+i))
+                    .extract().path("id");
+        }
+
+        System.out.println(Arrays.toString(cardsIdArr));
 
 
     }
